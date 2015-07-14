681dbeffbfb1864ee2b184fe3011fd74b967606f
kamrenz <kamren@developintelligence.com>
2015-07-14 11:06:05 -0400

    Updating comments

--- src/component/hello.js
+++ src/component/hello.js
@@ -10,21 +10,22 @@
 		/**
 		 *  React component lifecycle callback.
 		 *    - Executes exactly once during the lifecycle of the
-		 *      compoenet and sets the initial state of the component
+		 *      component and sets the initial state of the component
 		 *    - Executes before the component has been drawn on screen
 		 **/
 		getInitialState: function() {
-			//Initially default this data to empty
+			//Return an object literal containing component state
 			return {
 				place: 'World'
 			};
 		},
 		/**
-		 * React life-cycle method used to return this component.
+		 * React internal method used to render this component.
 		 *  Utilizes the properties and state placed into it.
+		 *  Only method needed to create a React component.
 		 */
 		render: function() {
-			//Rendered HTML
+			//Rendered HTML with the component state
 			return (
 				React.createElement("h1", null, "Hello ", this.state.place)
 			)
